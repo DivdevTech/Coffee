d192d48c7ac4ea3c817e661441cf61f6a4b96b9e
divdev <divdevtech@gmail.com>
2024-02-20 17:18:09 +0300

    full-app-routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-// import LoginPage from "../src/pages/loginPage";
-import CustomTable from "./components/table/CustomTable";
+import LoginPage from "../src/pages/loginPage";
+// import CustomTable from "./components/table/CustomTable";
 import "./styles/App.css";
 
 const App = () => {
   return (
     <div>
-      <CustomTable />
+      <LoginPage />
     </div>
   );
 };
